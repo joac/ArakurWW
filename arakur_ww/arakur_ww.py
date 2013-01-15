c702a163c1251f74177b5b34c0b46ae26ece1ead
Joaquín Sorianello <joac@joac.com.ar>
2013-01-15 02:51:09 -0300

    Ejemplo de graficas pusheadas desde el server

--- arakur_ww/arakur_ww.py
+++ arakur_ww/arakur_ww.py
@@ -1,19 +1,35 @@
-from flask import Flask, url_for, render_template
+import time
+import random
+
+from flask import Flask, url_for, render_template, Response, json
 from flask.ext.bootstrap import Bootstrap
 DEBUG = True
 SECRET_KEY = 'development key'
-
+BOOTSTRAP_JQUERY_VERSION = None
 app = Flask(__name__)
 
 app.config.from_object(__name__)
 
 Bootstrap(app)
 
+def event_stream():
+    while True:
+        a = {'oxigen':random.randint(0, 100), 'cloudiness': random.randint(0, 100), 'level': random.randint(0, 100)}
+        yield 'data: %s\n\n' % json.dumps(a)
+        time.sleep(0.5)
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/graficos')
+def graficos():
+    return render_template('graficos.html')
+
+@app.route('/stream')
+def stream():
+    return Response(event_stream(), mimetype="text/event-stream")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(threaded=True)
