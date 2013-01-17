bf6ac6fdf9b539bbf3f0083aeed930169241802a
Joaquín Sorianello <joac@joac.com.ar>
2013-01-17 01:09:52 -0300

    Agrego actualizacion de las  barras en la vista principal

--- arakur_ww/arakur_ww.py
+++ arakur_ww/arakur_ww.py
@@ -1,5 +1,6 @@
 import time
 import random
+import math
 
 from flask import Flask, url_for, render_template, Response, json
 from flask.ext.bootstrap import Bootstrap
@@ -12,11 +13,29 @@ app.config.from_object(__name__)
 
 Bootstrap(app)
 
-def event_stream():
+
+def generar_onda( componentes):
+    n = 1
     while True:
-        a = {'oxigen':random.randint(0, 100), 'cloudiness': random.randint(0, 100), 'level': random.randint(0, 100)}
+        total = 0
+        for index, value in enumerate(componentes):
+            if index % 2:
+                total += math.sin(n * value)
+            else:
+                total += math.cos(n * value)
+        yield int(50 + 50 * (total / len(componentes)))
+        n += 0.1
+        n %= 6
+
+def event_stream():
+    oxigeno = generar_onda([0.5, -0.25, 0.125])
+    turbiedad = generar_onda([0.5, 0.33, -0.66])
+    nivel = generar_onda([1, -0.5])
+
+    while True:
+        a = {'oxigen':oxigeno.next(), 'cloudiness': turbiedad.next(), 'level': nivel.next()}
         yield 'data: %s\n\n' % json.dumps(a)
-        time.sleep(0.5)
+        time.sleep(0.1)
 
 @app.route('/')
 def index():
@@ -32,4 +51,4 @@ def stream():
 
 
 if __name__ == '__main__':
-    app.run(threaded=True)
+    app.run(threaded=True, host='0.0.0.0')
