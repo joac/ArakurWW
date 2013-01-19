ec722e29e91b2aa17c441417f569def69e7fb319
Joaquín Sorianello <joac@joac.com.ar>
2013-01-19 00:18:06 -0300

    Separo las constantes en un archivo

--- arakur_ww/plc_interface.py
+++ arakur_ww/plc_interface.py
@@ -1,69 +1,6 @@
 #! -*-coding: utf8 -*-
 from pymodbus.client.sync import ModbusTcpClient as ModbusClient
-
-ERROR_CODE = 0x80
-M_ALARM_OFF = 23
-M_STOP_SBR = 30
-M_START_SBR = 31
-M_RESET_VOL = 32
-MARK_COUNT = 33
-REGISTER_COUNT = 33
-
-ACTUAL_PROGRAM = 8
-PROGRAM_POINTER = 9 #Registro incial para escribir programas
-
-MAX_VOL_SBR = 28
-
-OXIGEN_MIN = 5
-OXIGEN_MAX = 6
-
-CLOUDINESS_MAX= 7
-
-alarms = {
-        'No hay alarmas presentes': 0,
-        'Parada de emergencia': 1,
-        'Generador de emergencia en marcha': 2,
-        'Alto nivel en pozo de bombeo inicial': 3,
-        'Alto nivel en pileta equalizadora': 4,
-        'Alto nivel en reactor SBR': 5,
-        'Alto nivel en pozo de bombeo de salida': 6,
-        'Fallo Bomba de pozo inicial 1': 7,
-        'Fallo Bomba de pozo inicial 2': 8,
-        'Fallo Bomba transvase 1': 9,
-        'Fallo Bomba transvase 2': 10,
-        'Fallo aireador equalizadora': 11,
-        'Fallo aireador SBR 1': 12,
-        'Fallo aireador SBR 2': 13,
-        'Fallo válvula de descarga': 14,
-        'Fallo válvula de espumas': 15,
-        'Fallo dosificador de cloro': 16,
-        'Fallo bomba pozo salida 1': 17,
-        'Fallo bomba pozo salida 2': 18,
-        'Fallo bomba recirculadora': 19,
-        'Oxigeno disuelto fuera de rango': 20,
-        'Turbiedad fuera de rango': 21,
-        }
-
-notificaciones = {
-        'Reactor en carga aireada' : 25,
-        'Reactor en aireacion' : 26,
-        'Reactor en sedimentacion' : 27,
-        'Reactor en descarga' : 28,
-        'Reactor detenido' : 29,
-    }
-
-niveles = {
-        'level' : 0,
-        'oxigen' : 1,
-        'cloudiness' : 2,
-        'volumen_tratado' : 3,
-        'volumen_tratado_parcial' : 4,
-        'oxigen_min' : OXIGEN_MIN,
-        'oxigen_max' : OXIGEN_MAX,
-        'cloudiness_max' : CLOUDINESS_MAX,
-        'programa_actual' : ACTUAL_PROGRAM,
-        'volumen_sbr': MAX_VOL_SBR,
-    }
+from constants import *
 
 class ArakurPLC(ModbusClient):
     """Mantiene una interface de alto nivel con el PLC de operacion del SBR"""
@@ -160,6 +97,7 @@ class ArakurPLC(ModbusClient):
 
             response = self.write_registers(direccion, [carga_aireada, aireacion, sedimentacion, descarga])
             return response.function_code < ERROR_CODE
+
         return false
 
     def cambiar_programa(self, valor):
