2a59ad2c4eeaa063bce0667d27dc45c603467126
Joaquín Sorianello <joac@joac.com.ar>
2013-01-19 00:52:31 -0300

    Emprolijo codigo de utils y de la aplicacion

--- arakur_ww/arakur_ww.py
+++ arakur_ww/arakur_ww.py
@@ -1,17 +1,14 @@
-import time
-import random
-import math
 import redis
+import utils
 
 from flask import Flask, url_for, render_template, Response, json, jsonify
 from flask.ext.bootstrap import Bootstrap
-import utils
 
 DEBUG = True
 SECRET_KEY = 'development key'
 BOOTSTRAP_JQUERY_VERSION = None
-app = Flask(__name__)
 
+app = Flask(__name__)
 app.config.from_object(__name__)
 
 Bootstrap(app)
@@ -21,18 +18,6 @@ broker = redis.StrictRedis(host='localhost', port=6379, db=0)
 def enviar_comando(function_name, *args, **kwargs):
     return broker.publish('commands', utils.remote_plc_command(function_name, *args, **kwargs))
 
-def generar_onda( componentes):
-    n = 1
-    while True:
-        total = 0
-        for index, value in enumerate(componentes):
-            if index % 2:
-                total += math.sin(n * value)
-            else:
-                total += math.cos(n * value)
-        yield 50 + (50 * (total / len(componentes)) * math.cos(0.1*n))
-        n += 0.1
-        n %= 2000
 
 def event_stream():
     stream = broker.pubsub()
@@ -57,12 +42,14 @@ def stream():
 def iniciar_programa(programa):
     if programa in xrange(1, 5):
         enviar_comando('iniciar_programa', programa)
-
-    return "HEllooo"
+    return "enviado!"
 
 @app.route('/actualizar/<int:programa>', methods='POST')
 def actualizar_programa(programa):
     return programa
+
+
+#Debug methods, para poder escribir registro y marcas aleatorias del plc
 
 @app.route('/registro/<int:direccion>/<int:valor>/')
 def actualizar_registro(direccion, valor):
@@ -76,4 +63,5 @@ def actualizar_marca(direccion, valor):
     return "enviado!"
 
 if __name__ == '__main__':
+
     app.run(threaded=True, host='0.0.0.0')
--- arakur_ww/utils.py
+++ arakur_ww/utils.py
@@ -1,15 +1,37 @@
+#! -*-coding: utf8 -*-
+
 import json
+import math
 
 def remote_plc_command(function_name, *args, **kwargs):
-    print function_name
-    print args
-    print kwargs
-    data = {'name': function_name,
+    """Convierte los argumentos en un JSON, para poder llamar una
+    funcion remotamente usando pubsub de Redis"""
+
+    data = {
+            'name': function_name,
             'args': args,
             'kwargs' : kwargs,
             }
     return json.dumps(data)
 
 def execute_command(instance, message):
+    """Ejecuta un metodo de instance, a partir de un json generado
+    con remote_plc_command"""
+
     c = json.loads(message)
     return getattr(instance, c['name'])(*c['args'], **c['kwargs'])
+
+def generar_onda(componentes):
+    """Generador por sintesis para generar ondas complejas"""
+    n = 1
+    while True:
+        total = 0
+        for index, value in enumerate(componentes):
+            if index % 2:
+                total += math.sin(n * value)
+            else:
+                total += math.cos(n * value)
+        yield 50 + (50 * (total / len(componentes)) * math.cos(0.1*n))
+        n += 0.1
+        n %= 2000
+
